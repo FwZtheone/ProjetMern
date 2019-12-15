9ba010528c530eccd92558ca0a7992e9d2ce2ec7
fwztheone <fabrizio.villani@std.heh.be>
2019-12-15 17:19:35 +0100

    ajout de la fonction login et authentification

--- controllers/account/lib.js
+++ controllers/account/lib.js
@@ -27,8 +27,67 @@ async function signup(req,res){
          const findUser = await User.findOne({
              email
          });
-         if(findUser)
+         if(findUser){
+             res.status(400).json({
+                 text : "émail déjà utilisée"
+             })
+
+         }
+    
+
      }
+     catch (e){return res.status(500).json({e})}
+    try
+    {
+        //save l'user dans la DB
+        const userData = new User(user);
+        const userObject = await userData.save();
+        return res.status(200).json({
+            text :"succès",
+            token : userObject.getToken()
+        })
+    }
+    catch(e){res.status(500).json({e})}
 
     
+}
+
+
+
+async function(req,res){
+
+    const {password, email } = req.body;
+    if(!email ||!password){
+        res.status(400).json({
+            text: "requête invalide"
+        })
+    }
+
+    try{
+
+        //on tcheck si l'user existe dans la DB
+
+        const findUser = await User.findOne({email})
+        if(!findUser){
+            return res.status(400).json({
+                text: "l'user n'existe pas"
+            })
+
+
+        }
+        if(!findUser.authenticate(password)){
+            return res.status(400).json({
+                text :"le password est incorrect"
+            })
+        }
+        return res.status(200).json({
+            text:"authentification réussie",
+            token : findUser.getToken()
+        })
+
+
+    }
+    catch(e) {return res.status(500).json({e})}
+
+
 }
